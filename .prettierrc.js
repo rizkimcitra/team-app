0ce2a651aacea4fd74051e9ef14ce2bef6b4f977
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-05-13 15:17:09 +0700

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    tabWidth: 3,
+    printWidth: 120,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    trailingComma: "none",
+    arrowParens: "always",
+    endOfLine: "auto"
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: "app-root",
+   templateUrl: "./app.component.html",
+   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'team-app';
+   newMemberName = "";
+   errorMsg = "";
+   members: string[] = [];
+   numberOfTeams: number | "" = "";
+   teams: string[][] = [];
+
+   clearValue() {
+      this.newMemberName = "";
+   }
+
+   onInput(value: string) {
+      this.errorMsg && (this.errorMsg = "");
+      this.newMemberName = value;
+   }
+
+   onNumberOfTeamsInput(value: string) {
+      this.errorMsg && (this.errorMsg = "");
+      this.numberOfTeams = +value;
+   }
+
+   addMember() {
+      if (this.newMemberName.length > 0) {
+         this.members.push(this.newMemberName);
+         this.clearValue();
+      } else {
+         this.errorMsg = "Name can't be empty";
+      }
+   }
+
+   generateTeams() {
+      if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+         this.errorMsg = "invalid number of teams!";
+         return;
+      }
+
+      if (this.members.length < this.numberOfTeams) {
+         this.errorMsg = "Not enough members!";
+         return;
+      }
+
+      this.errorMsg = "";
+      const allMembers = [...this.members];
+
+      while (allMembers.length) {
+         for (let i = 0; i < this.numberOfTeams; i++) {
+            const randomIndex = Math.floor(Math.random() * allMembers.length);
+            const member = allMembers.splice(randomIndex, 1)[0];
+            if (!member) break;
+
+            if (this.teams[i]) {
+               this.teams[i].push(member);
+            } else {
+               this.teams[i] = [member];
+            }
+         }
+      }
+
+      this.members = [];
+   }
 }
